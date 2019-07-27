48cbf38c115b0d9e55b9f68f9bfa22e94aaab7bb
tubbytani <46688469+tubbytani@users.noreply.github.com>
2019-07-27 20:53:22 +0530

    Add files via upload

---
+++ temp_monitor.js
@@ -0,0 +1,8 @@
+setChartLibrary('google-chart');
+setChartTitle('TEMPERATURE MONITOR');
+setChartType('lineGraph');
+setAxisName('time_stamp','input');
+add(183);
+mul(0.0977);
+mul(1.8);
+plotChart('time_stamp','input');
